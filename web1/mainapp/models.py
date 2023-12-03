754731ba977abb45ffdab51007a6d59dc8ba1687
Admin <verysbond@ya.ru>
2023-12-03 18:05:03 +0300

    webfiles

--- web1/mainapp/models.py
+++ web1/mainapp/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-    title = models.CharField('Название', max_length=50),
-    task = models.TextField('Описание', max_length=250)
+    title = models.CharField('Название', max_length=50, default="default title")
+    task = models.TextField('Описание', max_length=250, blank=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    # def __str__(self):
+    #     return f'{self.title}'
+    #     # return self.title
--- web1/mainapp/views.py
+++ web1/mainapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 # Create your views here.
 
 def main_index(reqest):
-    return render(reqest, 'mainapp/index.html')
+    task = Task.objects.all()
+
+    return render(reqest, 'mainapp/index.html', {'title': 'Главная страница', 'tasks': task })
 
 def about(reqest):
     return render(reqest, 'mainapp/about.html')
