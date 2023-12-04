69b7899b67fb20979bba27f7fd091c28762e6fd9
Admin <verysbond@ya.ru>
2023-12-04 13:42:39 +0300

    webfiles

--- web1/mainapp/forms.py
+++ web1/mainapp/forms.py
@@ -1,10 +1,30 @@
-from .models import Task
-from django.forms import ModelForm, TextInput
+# from .models import Task
+# from django.forms import ModelForm, TextInput
+#
+# class TaskForm(ModelForm):
+#     class Meta:
+#         model = Task
+#         fields = ['title', 'task']
+#         widgets = {'title': TextInput(attrs={'class': "form-control", "placeholder": 'Введите название'}),
+#                    'task': TextInput(attrs={'class': "form-control", "placeholder": 'Введите описание'})
+#         }
+#
+#
 
-class TaskForm(ModelForm):
-    class Meta:
-        model = Task
-        fields = ['title', 'task']
-        widgets = {'title': TextInput(attrs={'class': "form-control", "placeholder": 'Введите название'}),
-                   'task': TextInput(attrs={'class': "form-control", "placeholder": 'Введите описание'})
-        }
+from django import forms
+
+
+class TaskForm(forms.Form):
+    title = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите название'
+        })
+    )
+    task = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ведите описание'
+        })
+    )
