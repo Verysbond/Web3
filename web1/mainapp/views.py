d3afc5d486abfdc91e6c0feeb6c9d96142ccae31
Admin <verysbond@ya.ru>
2023-12-04 13:00:51 +0300

    webfiles

--- web1/mainapp/views.py
+++ web1/mainapp/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 # Create your views here.
 
-def main_index(reqest):
+def main_index(request):
     task = Task.objects.all()
 
-    return render(reqest, 'mainapp/index.html', {'title': 'Главная страница', 'tasks': task })
+    return render(request, 'mainapp/index.html', {'title': 'Главная страница', 'tasks': task })
 
 
-def about(reqest):
-    return render(reqest, 'mainapp/about.html')
+def about(request):
+    return render(request, 'mainapp/about.html')
 
 
-def create(reqest):
-    return render(reqest, 'mainapp/create.html')
+def create(request):
+    form = TaskForm()
+    context = {
+        "form": form,
+    }
+    return render(request, 'mainapp/create.html', context)
 
